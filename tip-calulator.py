3433b1efd4c4031ab43096cd0b5c3a020c8aadf3
tiluu <tina.p.luu@gmail.com>
2013-07-28 23:32:22 -0400

    version 1

--- tip-calulator.py
+++ tip-calulator.py
@@ -1,13 +1,16 @@
  #! /usr/bin/env python 
-cost = {:.2f}.format(20.00)
-tax = {:.2f}.format(3.00)
-tip = {:.2f}.format(3.45)
-total = {:.2f}.format(26.45)
+cost = 20.00 #{:.2f}.format(20.00)
+tax = 3.00/100 #{:.2f}.format(3.00)
+tip = 15.00/100  #{:.2f}.format(3.45)
+tax_value = tax*cost
+meal_tax = cost+tax
+meal_tip = cost*tip
+total = meal_tax+meal_tip
 
-print ('The base cost of you meal was $%f') % (20.00)
-print ('You need to pay $%f for tax.') % (3.00)
-print ('Tipping at a rate of 15%%, you should leave $%f for a tip.') % (3.45)
-print ('The grand total of your meal is $%f') % (26.45)
+print ('The base cost of you meal was $%f') % (cost)
+print ('You need to pay $%f for tax.') % (tax)
+print ('Tipping at a rate of 15%%, you should leave $%f for a tip.') % (tip)
+print ('The grand total of your meal is $%f') % (total)
 
 
 print ('NEW FORMAT!!!!!---------------------')
