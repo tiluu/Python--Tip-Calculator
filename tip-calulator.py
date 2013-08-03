e24cac8a3db4bd57507762b60c36d1e2332542df
tiluu <tina.p.luu@gmail.com>
2013-08-03 15:15:09 -0400

    version2 with raw_input

--- tip-calulator.py
+++ tip-calulator.py
@@ -1,21 +1,25 @@
  #! /usr/bin/env python 
-cost = 20.00 #{':.2f'}.format(20.00)
-tax = 3.00/100 #{':.2f'}.format(3.00)
-tip = 15.00/100  #{':.2f'}.format(3.45)
+cost = float(raw_input('What is the cost of meal?'))
+tax = float(raw_input('What is the tax?')) 
+tip = float(raw_input('How much is tip?')) 
+tip_percentage = tip/100
+tax_percentage = tax/100
+
 tax_value = tax*cost
-meal_tax = cost+tax
-meal_tip = cost*tip
-total = meal_tax+meal_tip
+meal_tax = cost*tax_percentage
+meal_tip = cost*tip_percentage
+total = cost+meal_tax+meal_tip
 
 print ('The base cost of you meal was $%s') % '{:.2f}'.format(cost)
 print ('You need to pay $%s for tax.') % '{:.2f}'.format(tax)
-print ('Tipping at a rate of 15%%, you should leave $%s for a tip.') % '{:.2f}'.format(tip)
+print ('Tipping at a rate of 15%%, you should leave $%s for a tip.') % '{:.2f}'.format(meal_tip)
 print ('The grand total of your meal is $%s') % '{:.2f}'.format(total)
 
 
 print ('NEW FORMAT!!!!!---------------------')
 print ('The base cost of you meal was ${}').format('{:.2f}'.format(cost))
-print ('You need to pay ${} for tax.').format('{:.2f}'.format(tax))
-print ('Tipping at a rate of 15%, you should leave ${} for a tip.').format('{:.2f}'.format(tip))
+print ('You need to pay ${} for tax.').format('{:.2f}'.format(meal_tax))
+print ('Tipping at a rate of 15%, you should leave ${} for a tip.').format('{:.2f}'.format(meal_tip))
 print ('The grand total of your meal is ${}').format('{:.2f}'.format(total))
+
 
